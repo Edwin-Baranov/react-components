5f4fbb3c54f16a5ed1791fadb4cf6c9fe4566040
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-04 11:57:25 -0700

    Completed React Components

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      have: false,
+      hover: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      have: !this.state.have
+    });
+  }
+
+  onHoverState() {
+    this.setState({
+      hover: true
+    });
+  }
+
+  offHoverState() {
+    this.setState({
+      hover: false
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.have ? 'line-through' : 'none',
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+    return (<li style = {style} onClick={this.onListItemClick.bind(this)} onMouseEnter={this.onHoverState.bind(this)} onMouseLeave={(this.offHoverState.bind(this))}>{this.props.item}</li>);
+  }
+}
+
+
+var GroceryList = (props) => (
+  <>
+    <h2>Grocery List</h2>
+    <ul>
+      {props.groceries.map(grocery => <GroceryListItem item = {grocery}/>)}
+    </ul>
+  </>
+)
+
+var App = () => (
+    <GroceryList groceries={['Cucumbers', 'Kale']}/>
+);
+
+ReactDOM.render(<App/>, document.getElementById("app"));
